a98fe0e0d77a1275f6a754b98bb63a067d2ecee5
Nikita <thenromanov@gmail.com>
2020-04-24 22:17:07 +0300

    New edition

--- Alice.py
+++ Alice.py
@@ -36,10 +36,7 @@ def handleDialog(req, res):
     if any(argree in req['request']['original_utterance'].lower() for argree in ['ладно', 'куплю', 'покупаю', 'хорошо']):
         res['response']['text'] = f'{animals[0][0].upper() + animals[0][1:]}а можно найти на Яндекс.Маркете!'
         animals.pop(0)
-        if len(animals) > 0:
-            sessionStorage[userId] = {'suggests': ['Не хочу.', 'Не буду.', 'Отстань.']}
-            res['response']['text'] += f' А купи {animals[0]}а!'
-            res['response']['buttons'] = getSuggests(userId)
+        res['session']['new'] = True
         res['response']['end_session'] = len(animals) == 0
         return
     res['response']['text'] = f"Все говорят '{req['request']['original_utterance']}', а ты купи {animals[0]}а!"
